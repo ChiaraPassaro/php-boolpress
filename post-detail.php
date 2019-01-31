1b2594d10cc93e16a7df4acf0b06e5367ae2004a
Chiara Passaro <chiarapassaro.wd@gmail.com>
2019-01-31 18:44:25 +0100

    Add form

--- post-detail.php
+++ post-detail.php
@@ -5,41 +5,41 @@
 
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-</head>
-<body>
-    <?php
-        $slug_get = $_GET['slug'];
-            foreach ($posts as $post) {
-                if ($slug_get === $post['slug']){
-                    $title = $post['title'];
-                    $date = getFormatDate($post['published_at']);
-                    $img = $post['image'];
-                    $content = $post['content'];
-                    $tags = implode(', ', $post['tag']);
-                    $is_post = true;
-                } else{
-                 $message_error = 'La pagina non esiste';
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $slug_get = $_GET['slug'];
+                foreach ($posts as $post) {
+                    if ($slug_get === $post['slug']){
+                        $title = $post['title'];
+                        $date = getFormatDate($post['published_at']);
+                        $img = $post['image'];
+                        $content = $post['content'];
+                        $tags = implode(', ', $post['tag']);
+                        $is_post = true;
+                    } else{
+                     $message_error = 'La pagina non esiste';
+                    }
                 }
-            }
-    ?>
+        ?>
 
-    <?php if($is_post){ ?>
-        <div class="post-detail">
-            <h1 class="post__title"><?php echo $title; ?></h1>
-            <div class="post__date"><?php echo $date; ?></div>
-            <div class="post__img"><img src="<?php echo $img; ?>" alt="<?php echo $title; ?>"></div>
-            <div class="post__content"><?php echo $content; ?></div>
-            <div class="post__tag">Tags: <?php echo $tags; ?></div>
-        </div>
-    <?php } else { ?>
-        <h1 class="error"><?php echo $message_error; ?></h1>
-    <?php } ?>
+        <?php if($is_post){ ?>
+            <div class="post-detail">
+                <h1 class="post__title"><?php echo $title; ?></h1>
+                <div class="post__date"><?php echo $date; ?></div>
+                <div class="post__img"><img src="<?php echo $img; ?>" alt="<?php echo $title; ?>"></div>
+                <div class="post__content"><?php echo $content; ?></div>
+                <div class="post__tag">Tags: <?php echo $tags; ?></div>
+            </div>
+        <?php } else { ?>
+            <h1 class="error"><?php echo $message_error; ?></h1>
+        <?php } ?>
 
-</body>
+    </body>
 </html>
--- posts.php
+++ posts.php
@@ -8,32 +8,38 @@
 
 <!doctype html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts</title>
-</head>
-<body>
-    <h1 class="page__title">Tutti i Post</h1>
-    <?php
-    if(count($posts) > 0){
-        foreach ($posts as $post){
-                $slug = $post['slug'];
-                $title = $post['title'];
-                $content = $post['content'];
-                $content = substr ( $content , 0 , 150);
-                $date = getFormatDate($post['published_at']);
-            ?>
-            <div class="post">
-                <a href="post-detail.php/?slug=<?php echo $slug; ?>"><h2 class="post_title"><?php echo $title; ?></h2></a>
-                <div class="post_date"><?php echo $date; ?></div>
-                <div class="post_content"><?php echo $content; ?> ...</div>
-            </div>
-    <?php }
-    } else { ?>
-        <h1 class="error"><?php echo 'Non sono presenti post' ?></h1>
-    <?php }?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Posts</title>
+    </head>
+    <body>
+        <h1 class="page__title">Tutti i Post</h1>
+        <form action="posts.php" method="get" class="filter">
+            <label for="tag">Inserisci un tag</label>
+            <input name="tag" type="text" class="filter__tag">
+            <button type="submit">Invia</button>
+        </form>
+
+        <?php
+        if(count($posts) > 0){
+            foreach ($posts as $post){
+                    $slug = $post['slug'];
+                    $title = $post['title'];
+                    $content = $post['content'];
+                    $content = substr ( $content , 0 , 150);
+                    $date = getFormatDate($post['published_at']);
+                ?>
+                <div class="post">
+                    <a href="post-detail.php/?slug=<?php echo $slug; ?>"><h2 class="post_title"><?php echo $title; ?></h2></a>
+                    <div class="post_date"><?php echo $date; ?></div>
+                    <div class="post_content"><?php echo $content; ?> ...</div>
+                </div>
+        <?php }
+        } else { ?>
+            <h1 class="error"><?php echo 'Non sono presenti post' ?></h1>
+        <?php }?>
+    </body>
 </html>
